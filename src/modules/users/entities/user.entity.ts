b6c4baff7e59b9f927d8142601b66b6468582ed9
SRT DEV <sahirruiz4@gmail.com>
2026-05-22 02:08:52 -0500

    feat: Agregar entidad de usuario con propiedades y columnas necesarias

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,49 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ unique: true })
+  email?: string;
+
+  @Column({ unique: true })
+  username?: string;
+
+  @Column()
+  password?: string;
+
+  @Column()
+  firstName?: string;
+
+  @Column()
+  lastName?: string;
+
+  @Column({ nullable: true })
+  avatar?: string;
+
+  @Column({ nullable: true, type: 'text' })
+  bio?: string;
+
+  @Column({ default: 'ACTIVE' })
+  status?: string;
+
+  @Column({ type: 'int' })
+  roleId?: number;
+
+  @Column({ type: 'int' })
+  universityId?: number;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
+}
