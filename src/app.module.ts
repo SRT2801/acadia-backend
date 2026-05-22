5e548f856282eb5957c17016d3fe333fa75b6899
SRT DEV <sahirruiz4@gmail.com>
2026-05-22 02:18:54 -0500

    feat: Configurar módulos de autenticación, usuarios, roles y base de datos en el módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { RolesModule } from './modules/roles/roles.module';
+import { DatabaseModule } from './core/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UsersModule,
+    RolesModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
