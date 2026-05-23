0631a51c3520deebbaf5df34d4d3d9017b58f5fe
SRT DEV <sahirruiz4@gmail.com>
2026-05-23 17:59:30 -0500

    feat(cors): eliminar valor por defecto en la configuración de origen

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? 'http://localhost:4200',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   });
 
