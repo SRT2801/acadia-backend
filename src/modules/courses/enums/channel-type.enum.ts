676dedc89e931a28565ac551e7ff79eb667ad78e
SRT DEV <sahirruiz4@gmail.com>
2026-05-26 13:48:55 -0500

    feat(courses): agregar enumeración ChannelType para la gestión de tipos de canales

---
+++ src/modules/courses/enums/channel-type.enum.ts
@@ -0,0 +1,10 @@
+export enum ChannelType {
+  TEXT = 'TEXT',
+  ANNOUNCEMENT = 'ANNOUNCEMENT',
+  TASKS = 'TASKS',
+  VOICE = 'VOICE',
+  FORUM = 'FORUM',
+  RESOURCES = 'RESOURCES',
+}
+
+export const ALL_CHANNEL_TYPES = Object.values(ChannelType);
