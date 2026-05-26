ee004657d87f9c5573bf3a434be36fcb6bcf052d
SRT DEV <sahirruiz4@gmail.com>
2026-05-26 13:45:30 -0500

    feat(courses): agregar enumeración para el estado de los miembros del curso

---
+++ src/modules/courses/enums/course-member-status.enum.ts
@@ -0,0 +1,8 @@
+export enum CourseMemberStatus {
+  ACTIVE = 'ACTIVE',
+  PENDING = 'PENDING',
+  SUSPENDED = 'SUSPENDED',
+  LEFT = 'LEFT',
+}
+
+export const ALL_COURSE_MEMBER_STATUSES = Object.values(CourseMemberStatus);
