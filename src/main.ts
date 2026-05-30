25cdd0f7ba0ced30408ac815afd123245e9ff4be
SRT DEV <sahirruiz4@gmail.com>
2026-05-30 01:38:14 -0500

    feat(swagger): configurar Swagger con titulo, descripcion y autenticacion por cookies

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './core/filters/all-exceptions.filter';
 import cookieParser from 'cookie-parser';
@@ -26,6 +27,27 @@ async function bootstrap() {
 
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Acadia API')
+    .setDescription(
+      'Academic platform API for course management, collaboration, and communication',
+    )
+    .setVersion('1.0')
+    .addCookieAuth(
+      'accessToken',
+      { type: 'apiKey', in: 'cookie' },
+      'accessToken',
+    )
+    .addCookieAuth(
+      'refreshToken',
+      { type: 'apiKey', in: 'cookie' },
+      'refreshToken',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
