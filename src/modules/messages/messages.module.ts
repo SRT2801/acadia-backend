b545f619182932e9364d65c15f7a34f9683a0bfe
SRT DEV <sahirruiz4@gmail.com>
2026-05-30 15:27:52 -0500

    feat(messages): integrar NotificationsModule y CoursesModule

--- src/modules/messages/messages.module.ts
+++ src/modules/messages/messages.module.ts
@@ -5,12 +5,16 @@ import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
 import { ChannelsModule } from '../channels/channels.module';
 import { ChatModule } from '../chat/chat.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { CoursesModule } from '../courses/courses.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Message]),
     forwardRef(() => ChannelsModule),
     forwardRef(() => ChatModule),
+    forwardRef(() => NotificationsModule),
+    forwardRef(() => CoursesModule),
   ],
   controllers: [MessagesController],
   providers: [MessagesService],
